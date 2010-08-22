fcc9f852e2e08c41591332c1e1b29f60133d3093
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2010-08-22 13:43:07 -0600

    Added flask templating support. Html5 focus of Jinja2 has flushed out a design flaw in our brushes that convert everything to lxml trees -- HTML5 is simply not XHTML

---
+++ ext/flask/template.py
@@ -0,0 +1,74 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+##~ Copyright (C) 2002-2010  TechGame Networks, LLC.              ##
+##~                                                               ##
+##~ This library is free software; you can redistribute it        ##
+##~ and/or modify it under the terms of the MIT style License as  ##
+##~ found in the LICENSE file included with this distribution.    ##
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Imports 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import flask
+
+from pyseas import WebComponent
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Definitions 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class FlaskTemplate(WebComponent):
+    context = {}
+
+    def __init__(self, **context):
+        if context:
+            self.context = self.updateContext(context, False)
+
+    def renderHtmlOn(self, html):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+
+    def renderTemplate(self, html, templateName, **context):
+        context = self.updateContext(context, html=html)
+        return flask.render_template(templateName, **context)
+
+    def renderTemplateString(self, html, source, **context):
+        context = self.updateContext(context, html=html)
+        return flask.render_template_string(source, **context)
+
+    def updateContext(self, context, selfKey='obj', **kw):
+        ctx = self.context.copy()
+        if selfKey:
+            ctx[selfKey] = self
+        ctx.update(context)
+        ctx.update(kw)
+        return ctx
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class FlaskTemplateSource(FlaskTemplate):
+    source = None
+    def __init__(self, source=None, **context):
+        if source is not None:
+            self.source = source
+        if context:
+            self.context = self.updateContext(context, False)
+
+    def renderHtmlOn(self, html):
+        return self.renderTemplateString(html, self.source)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class FlaskNamedTemplate(FlaskTemplate):
+    templateName = None
+    def __init__(self, templateName=None, **context):
+        if templateName is not None:
+            self.templateName = templateName
+        if context:
+            self.context = self.updateContext(context, False)
+
+    def renderHtmlOn(self, html):
+        return self.renderTemplate(html, self.templateName)
+
+
